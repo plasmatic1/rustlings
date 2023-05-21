8bf6a52edf1b66f5c78260a12511a06d49e9d2b9
Moses Xu <moses@mosesxu.net>
2023-05-21 02:34:57 -0400

    work so far

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,41 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
-
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
+
+
+// Original function
+// // Should take ownership
+// fn string_uppercase(mut data: &String) {
+//     data = &data.to_uppercase();
+
+//     println!("{}", data);
+// }
+
+// - `mut` can have different meanings depending on where it is placed (like C++!)
+//   - ex. mut data: &mut String
+//     - The first `mut` refers to the fact that the binding (i.e. name) is mutable.  If only that mut was present, then we could change the value that the reference is pointing to, but not the reference itself
+//       - Note that & (and * as well) are low precedent, meaning that in line 31, the & applies after the to_uppercase
+//     - The second `mut` refers to the fact that the underlying memory at the reference is mutable.  If only that mut was present, then we could change the variable at the location (i.e. append to 'data') but not where it points to
+
+// In this original code, we intend for the reference `data` to always last at least as long as it's being used (AKA for all of string_uppercase)
+//  - An issue is that data.to_uppercase() creates a new string variable, whose ownership is transferred on return.  However, since it is never moved to a concrete variable and instead 'stays' as a temporary value
+// There is no direct equivalent of this behaviour in C++ (except maybe returning a heap-allocated object)
--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -6,8 +6,6 @@
 // before the parentheses on each line. If you're right, it will compile!
 // No hints this time!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -16,14 +14,22 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    string("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
+
+// String vs str (commonly &str) in rust (vs C++)
+// String <-> std::string
+    // Like std::string.  In C++, memory is allocated on the heap and automatically managed by the lifetime of the string object.  Contains all of the bells and whistles you'd normally expect anyway.  In rust, the only major difference is that lifetime is managed in the Rusty way, but the object still owns the underlying string
+// &str <-> char*
+    // Can correspond to an 'owned' string, but is often (in rust) just a view into a string owned by somebody else.  These strings have a couple more functions though, and use UTF8 by default
+
+// Note that different functions return &str and others return String.  This is due to optimization reasons.  Some string functions (like trim) have return values that will always substring the original string, so those can be returned as a reference for efficiency reasons, while other functions (like replace) destructively modify the string, and thus must be copied and the new string must have its own ownership properties
